15428d04c66ec914ecbc1709521f4396bf696cc9
Yonko-Kunal <your-email@example.com>
2025-10-30 00:39:59 +0530

    project landing implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Container from "@/components/common/Container";
 import Hero from "@/components/landing/Hero";
+import Projects from "@/components/landing/Projects";
 
 export default function Home() {
   return (
     <Container className="min-h-screen py-16">
       <Hero />
+      <Projects />
 
     </Container>
   );
---
+++ src/config/Footer.tsx
@@ -0,0 +1,5 @@
+export const footerConfig = {
+    developer: 'Yonko-Kunal',
+    text: 'Design & Developed by',
+    copyright: 'All rights reserved.',
+};
