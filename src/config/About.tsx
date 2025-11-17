94fe47ecb978e9c73f964d126bf7a156c1cec904
Yonko-Kunal <128897093+Yonko-Kunal@users.noreply.github.com>
2025-11-17 23:16:43 +0530

    config modification

--- src/config/About.tsx
+++ src/config/About.tsx
@@ -22,5 +22,5 @@ export const mySkills = [
 
 export const about = {
     name: 'Kunal Roy',
-    description: `I'm a Full Stack web developer and Open Source Contributor, I love building products to solve real-world problems. I'm specialized in building MVP's.`,
+    description: `I'm a Full Stack web developer, I love building products to solve real-world problems. I'm specialized in building MVP's.`,
 };
