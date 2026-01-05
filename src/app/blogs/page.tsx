483b945a48cc64ae35adf60a45b3f8754c5ed4f5
Yonko-Kunal <128897093+Yonko-Kunal@users.noreply.github.com>
2026-01-05 08:17:24 +0530

    ADD : Empty blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex h-screen items-center justify-center text-lg md:text-xl">
+      <h1>No blog posts yet</h1>
+    </div>
+  );
+};
+
+export default page;
